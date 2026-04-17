3e8d28c1b41e8dd0a71b27125dd26298fbcc7e1a
init-24782067-ade irma sari <adeirmasari255@gmail.com>
2026-04-17 17:49:13 +0700

    Lab5_Alert feedback menggunakan Django messages

--- iet_24782067_2026/main_app/views.py
+++ iet_24782067_2026/main_app/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.urls import reverse_lazy
 from .models import Report
 from .forms import ReportForm
+from django.contrib import messages
 
 
 # HOME 
@@ -31,6 +32,10 @@ class ReportCreateView(CreateView):
     template_name = 'main_app/add_report.html'
     success_url = reverse_lazy('report_list')
 
+    def form_valid(self, form):
+        messages.success(self.request, "Laporan berhasil ditambahkan.")
+        return super().form_valid(form)
+
 
 # UPDATE
 class ReportUpdateView(UpdateView):
@@ -39,12 +44,20 @@ class ReportUpdateView(UpdateView):
     template_name = 'main_app/update_report.html'
     success_url = reverse_lazy('report_list')
 
+    def form_valid(self, form):
+        messages.success(self.request, "Laporan berhasil diperbarui.")
+        return super().form_valid(form)
+
 
 # DELETE
 class ReportDeleteView(DeleteView):
     model = Report
     template_name = 'main_app/delete.html'
     success_url = reverse_lazy('report_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Laporan berhasil dihapus.")
+        return super().delete(request, *args, **kwargs)
 
 
 # WORKFLOW STATUS
@@ -55,12 +68,15 @@ class ReportUpdateStatusView(View):
 
         if report.status == 'REPORTED' and new_status == 'VERIFIED':
             report.status = 'VERIFIED'
+            messages.success(request, "Laporan berhasil diverifikasi.")
 
         elif report.status == 'VERIFIED' and new_status == 'IN_PROGRESS':
             report.status = 'IN_PROGRESS'
+            messages.success(request, "Laporan sedang diproses.")
 
         elif report.status == 'IN_PROGRESS' and new_status == 'RESOLVED':
             report.status = 'RESOLVED'
+            messages.success(request, "Laporan telah diselesaikan.")
 
         report.save()
         return redirect('report_list')
