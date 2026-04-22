bff2f0c03f858cd839d8702c50fbb8979f0c95e0
init-24782067-ade irma sari <adeirmasari255@gmail.com>
2026-04-22 14:58:18 +0700

    Lab6_Extend AbstractUser dan tambahkan field,Daftarkan AUTH_USER_MODEL di settings.py

--- iet_24782067_2026/usermanagement_24782067/models.py
+++ iet_24782067_2026/usermanagement_24782067/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_member = models.BooleanField(default=True)
