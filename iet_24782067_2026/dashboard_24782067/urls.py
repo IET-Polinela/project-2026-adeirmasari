7839b78f2e218fc261d14eb1f1de44bc120298ab
init-24782067-ade irma sari <adeirmasari255@gmail.com>
2026-04-29 14:27:15 +0700

    Lab7_Implementasi Backend

--- iet_24782067_2026/dashboard_24782067/urls.py
+++ iet_24782067_2026/dashboard_24782067/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, dashboard_stats
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('stats/', dashboard_stats, name='dashboard_stats'),
 ]
--- iet_24782067_2026/dashboard_24782067/views.py
+++ iet_24782067_2026/dashboard_24782067/views.py
@@ -1,4 +1,15 @@
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+
+
+def dashboard_stats(request):
+    data = {
+        'total_user': 120,
+        'total_admin': 5,
+        'total_citizen': 100,
+        'total_guest': 15
+    }
+    return JsonResponse(data)
 
 class DashboardView(TemplateView):
-    template_name = 'dashboard/dashboard.html'
+    template_name = 'dashboard.html'
