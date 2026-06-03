734545cf95f6517cbe328081e9a36cb10b06a790
init-24782067-ade irma sari <adeirmasari255@gmail.com>
2026-06-03 14:05:00 +0700

    Lab12_API Optimization: Pagination, Sorting, & Serializer

--- iet_24782067_2026/main_app/api_views.py
+++ iet_24782067_2026/main_app/api_views.py
@@ -1,19 +1,51 @@
 from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 
 from .models import Report
 from .serializers import ReportSerializer
 from .permissions import IsOwnerAndDraftOrReadOnly
 
 
-class ReportViewSet(viewsets.ModelViewSet):
+# Pagination
+class ReportPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-    queryset = Report.objects.all()
+
+class ReportViewSet(viewsets.ModelViewSet):
     serializer_class = ReportSerializer
+    pagination_class = ReportPagination
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # Sorting berdasarkan update terbaru
+        queryset = Report.objects.all().order_by('-updated_at')
+
+        # Filtering berdasarkan tab
+        tab = self.request.query_params.get('tab', None)
+
+        if tab == 'my_reports':
+            queryset = queryset.filter(reporter=user)
+
+        elif tab == 'feed':
+            queryset = queryset.filter(
+                ~Q(reporter=user) &
+                ~Q(status='DRAFT')
+            )
+
+        else:
+            queryset = queryset.filter(
+                ~Q(status='DRAFT') |
+                Q(status='DRAFT', reporter=user)
+            )
+
+        return queryset
 
     def get_permissions(self):
-
         if self.action in ['update', 'partial_update', 'destroy']:
-
             return [
                 permissions.IsAuthenticated(),
                 IsOwnerAndDraftOrReadOnly()
@@ -22,5 +54,9 @@ class ReportViewSet(viewsets.ModelViewSet):
         return [permissions.IsAuthenticated()]
 
     def perform_create(self, serializer):
-
         serializer.save(reporter=self.request.user)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
